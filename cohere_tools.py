fcb2235e2f4d5d5c9434442d9aba1f7d599a7f49
Alter-xyz <88554920+alterxyz@users.noreply.github.com>
2024-07-06 21:35:00 -0400

    feat: cohere tools and agent in stream mode

--- cohere_tools.py
+++ cohere_tools.py
@@ -7,12 +7,7 @@ from langchain_community.utilities.wolfram_alpha import WolframAlphaAPIWrapper
 
 
 """
-pip install wolframalpha
-pip install cohere
-pip install langchain langchain_cohere langchain_experimental
-pip install langchain-core
-pip install langchain-community
-pip install langchain-cohere
+pip install cohere wolframalpha duckduckgo-search langchain-community
 """
 
 # Initialize Cohere client
@@ -32,11 +27,8 @@ def step(describe: str) -> None:
 
 
 def web_search(query: str) -> str:
-    step("DuckDuckGo Search Start")
     search = DuckDuckGoSearchResults()
-    print(f"\n------\nDuck Query: {query}\n------\n")
-    result = search.run(query)
-    return result
+    return search.run(query)
 
 
 web_search_tool = {
@@ -53,18 +45,13 @@ web_search_tool = {
 
 
 def wolfram_alpha(query: str) -> str:
-    step("Wolfram Alpha API")
     wolfram = WolframAlphaAPIWrapper()
     try:
         lines = query.strip().split("\n")  # LLM tool use issue...
         processed_query = lines[-1] if len(lines) == 1 else lines[-1]
-
-        print(f"\n------\nOriginal Query: {query}\n")
-        print(f"Processed Query: {processed_query}\n------\n")
-
         result = wolfram.run(processed_query)
     except Exception as e:
-        result = f"Wolfram Alpha wasn't able to answer it: {str(e)}. Maybe try simplifying the question."
+        result = f"Wolfram Alpha wasn't able to answer. Try simplifying the question."
     return result
 
 
@@ -99,13 +86,11 @@ calculator_tool = {
 
 
 def chat_with_tools(message: str, model: str = model):
-    step("Run general chat")
     tools = [web_search_tool, wolfram_alpha_tool, calculator_tool]
     res = co.chat(model=model, message=message, force_single_step=False, tools=tools)
 
     while res.tool_calls:
-        step(f"Run tool calls:\n{res}\n")
-        print(f"Steps{STEP}: {res.text}\n")
+        print(res.text)
         tool_results = []
         for call in res.tool_calls:
             try:
@@ -144,10 +129,109 @@ def chat_with_tools(message: str, model: str = model):
     return res.text
 
 
+def chat_stream_with_tools(message, model=model):
+    tools = [web_search_tool, wolfram_alpha_tool, calculator_tool]
+    res = co.chat_stream(
+        model=model, message=message, force_single_step=False, tools=tools
+    )
+    # We try get the tool call, or the text generation. Since we had force_single_step=False, so either one of them will be returned.
+    for event in res:
+        if event.event_type == "stream-start":
+            pass
+        elif event.event_type == "tool-calls-chunk":
+            if hasattr(event, "text") and event.text:
+                print(event.text, end="")
+        elif event.event_type == "text-generation":
+            print(event.text, end="")
+        elif event.event_type == "stream-end":
+            end_event = event.response
+            print("\n\n")
+    res = end_event
+    (
+        print("\n#####\n#YES#\n#####\n")
+        if res.tool_calls
+        else print("\n#####\n#END#\n#####\n")
+    )
+    while res.tool_calls:
+        # print(res.text) # This will be an observation and a plan with next steps
+        tool_results = []
+        for call in res.tool_calls:
+            try:
+                if call.name == "web_search":
+                    co_result = [{"answer": web_search(call.parameters["query"])}]
+                elif call.name == "good_calculator":
+                    print(f"\n------\nQuery: {call}\n------\n")
+                    co_result = [{"answer": wolfram_alpha(call.parameters["query"])}]
+                elif call.name == "my_calculator":
+                    co_result = [
+                        {"answer": my_calculator(call.parameters["expression"])}
+                    ]
+                else:
+                    co_result = [{"answer": f"Unknown tool: {call.name}"}]
+            except Exception as e:
+                co_result = [
+                    {
+                        "answer": f"Error in tool: {call.name}: {e}. Skip this tool call in the following steps."
+                    }
+                ]
+
+            result = {"call": call, "outputs": co_result}
+            tool_results.append(result)
+            step(f"Current tool_results\n{tool_results}")
+
+            # call chat again with tool results
+        res = co.chat_stream(
+            model=model,
+            chat_history=res.chat_history,
+            message="",
+            force_single_step=False,
+            tools=tools,
+            tool_results=tool_results,
+        )
+        for event in res:
+            if event.event_type == "stream-start":
+                pass
+            elif event.event_type == "tool-calls-chunk":
+                if hasattr(event, "text") and event.text:
+                    print(event.text, end="")
+            elif event.event_type == "text-generation":
+                print(event.text, end="")
+            elif event.event_type == "stream-end":
+                end_event = event.response
+                print("\n\n")
+        res = end_event
+        (
+            print("\n#####\n#YES#\n#####\n")
+            if res.tool_calls
+            else print("\n#####\n#END#\n#####\n")
+        )
+
+
+def cohere_stream():
+    # co = cohere.Client("<<apiKey>>")
+
+    response = co.chat_stream(
+        chat_history=[
+            {"role": "USER", "message": "Who discovered gravity?"},
+            {
+                "role": "CHATBOT",
+                "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton",
+            },
+        ],
+        message="What year was he born?",
+        # perform web search before answering the question. You can also use your own custom connector.
+        connectors=[{"id": "web-search"}],
+    )
+
+    for event in response:
+        if event.event_type == "text-generation":
+            print(event.text, end="")
+
+
 # Example usage
 if __name__ == "__main__":
     user_message = "What's the population of the capital of France? Then calculate the square root of its population, round to 1 decimal place."
-    response = chat_with_tools(user_message)
+    response = chat_stream_with_tools(user_message)
     print("Final response:", response)
     while True:
         user_message = input("Enter your message: ")
